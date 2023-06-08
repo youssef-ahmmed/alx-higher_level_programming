3cbb7b9413cdb5480e8dfe4bfd6bf7fe66a6fa87
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-06-08 19:46:13 +0200

    Edit task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 a: int = 1
 b: int = 2
 
-print('{:d} + {:d} = {:d}'.format(a, b, add_0.add(a, b)))
+print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
