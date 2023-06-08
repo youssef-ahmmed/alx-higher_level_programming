e9d89a81c536da9f1086ad9d0f018b696404da89
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-06-08 20:10:16 +0200

    Add task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a: int = 10
+    b: int = 5
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
