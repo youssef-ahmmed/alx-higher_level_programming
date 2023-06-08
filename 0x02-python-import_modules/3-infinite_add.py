d810fc8004e301f590e1181e25fb9f541f3930d6
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-06-08 21:30:21 +0200

    Add task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+
+def infinite_add(argv, n):
+    sum = 0
+    for i in range(0, n):
+        sum += argv[i]
+
+    print(sum)
+
+
+if __name__ == "__main__":
+    
+    n = len(sys.argv) - 1
+    int_argument = [int(arg) for arg in sys.argv[1:]]
+    if n == 0:
+        print('{:d}'.format(0))
+    else:
+        infinite_add(int_argument, n)
