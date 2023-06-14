3b20ffeb33ac30e232e12ba21bb781daa1129aef
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-06-14 21:18:10 +0300

    Edit pycodestyle of task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -6,6 +6,6 @@ def complex_delete(a_dictionary, value):
             keys_to_delete.append(k)
 
     for key in keys_to_delete:
-        del(a_dictionary[key])
+        del (a_dictionary[key])
 
     return a_dictionary
