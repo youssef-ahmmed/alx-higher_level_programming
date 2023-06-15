df09943d6c7c8a6fea2582265aaa12618f6dac0b
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-06-15 07:28:04 +0300

    Edit task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,4 +3,4 @@ def weight_average(my_list=[]):
     from functools import reduce
     sum_product = reduce(lambda acc, val: acc + (val[0] * val[1]), my_list, 0)
     sum_weights = reduce(lambda acc, val: acc + val[1], my_list, 0)
-    return float(sum_product / sum_weights)
+    return float(sum_product / sum_weights) if sum_weights != 0 else 0
