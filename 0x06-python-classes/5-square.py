dd6eef62e259bbd531d9573bf223bd75fdddc734
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-06-29 10:15:27 +0300

    Add task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Defines a Square Class"""
+
+
+class Square:
+    """Simple class representing a square"""
+
+    def __init__(self, size=0):
+        """
+        Initialize a person with the given name and age
+
+        Args:
+            size (int): Size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter the current size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter the current size with some constrains"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Compute the area of the square
+
+        Return:
+            int: current square area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints in stdout the square with the character `#`"""
+        print("") if self.__size == 0 \
+            else [print('#' * self.__size) for _ in range(self.__size)]
