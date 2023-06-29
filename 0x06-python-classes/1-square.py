1e5caf6096d4c0117a404c6a9f57edc2d38b7b23
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-06-29 09:26:14 +0300

    Add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a Square Class"""
+
+
+class Square:
+    """Simple class representing a square"""
+    def __init__(self, size):
+        """
+        Initialize a person with the given name and age
+
+        Args:
+            size (int): Size of the square
+        """
+        self.__size = size
