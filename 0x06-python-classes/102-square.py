b4aa1a485268bea71b5bc9d273c8acef7b78f734
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-06-30 06:56:39 +0300

    Edit docstring for some methods

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -36,20 +36,26 @@ class Square:
         """
         return self.__size ** 2
 
-    def __lt__(self, other):
-        return self.area() < other.area()
-
-    def __le__(self, other):
-        return self.area() <= other.area()
-
     def __eq__(self, other):
+        """Define the == comparison to a Square."""
         return self.area() == other.area()
 
     def __ne__(self, other):
+        """Define the != comparison to a Square."""
         return self.area() != other.area()
 
-    def __ge__(self, other):
-        return self.area() > other.area()
+    def __lt__(self, other):
+        """Define the < comparison to a Square."""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """Define the <= comparison to a Square."""
+        return self.area() <= other.area()
 
     def __gt__(self, other):
+        """Define the > comparison to a Square."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Define the >= comparison to a Square."""
         return self.area() >= other.area()
