9cf898dcb89d33c667912a1a4e0f2500f8c94177
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-07-10 13:22:52 +0300

    Add task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for `inherits_from()` function"""
+
+
+def inherits_from(obj, a_class):
+    """function that returns True if the object is an instance
+        of a class that inherited (directly or indirectly)
+        from the specified class ; otherwise False"""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
