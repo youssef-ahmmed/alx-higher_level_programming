bd58c514ea2a795a0f2083f929cb52055c44d3bc
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-07-10 13:33:20 +0300

    Add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for `BaseGeometry` class"""
+
+
+class BaseGeometry:
+    """A base class for geometric shapes"""
+
+    def area(self):
+        """Calculates the area of the geometric shape"""
+        raise Exception("area() is not implemented")
