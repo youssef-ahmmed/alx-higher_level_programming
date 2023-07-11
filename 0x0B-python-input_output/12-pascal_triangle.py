302eb54ad7e53088282ad3e2d5173284ad5b03d8
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-07-12 01:13:38 +0300

    Add Mandatory task (12)

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module for computing Pascal Triangle"""
+
+
+def pascal_triangle(n):
+    """function that represents the Pascal’s triangle"""
+    pascal_list = [[]]
+    new_row = []
+    if n <= 0:
+        return pascal_list
+    pascal_list.append([1])
+
+    for i in range(n - 1):
+        middle_values = sum_index(new_row)
+        new_row = [1] + middle_values + [1]
+        pascal_list.append(new_row)
+
+    return pascal_list[1:]
+
+
+def sum_index(lst):
+    """Calculate the middle values of Pascal’s triangle"""
+    new_list = []
+    for idx in range(0, len(lst) - 1):
+        new_list.append(lst[idx] + lst[idx + 1])
+    return new_list
