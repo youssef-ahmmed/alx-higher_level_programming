691b7cef89ef5bdb5b9bc841e45d6f6f865b1369
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-07-11 22:14:55 +0300

    Edit Mandatory task (7)

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-import json
+"""Module for passing argument to python list"""
 import sys
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -9,9 +8,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 if __name__ == '__main__':
 
-    file_name = "files/add_item.json"
+    FILENAME = "files/add_item.json"
     try:
-        my_list = load_from_json_file(file_name)
+        my_list = load_from_json_file(FILENAME)
     except FileNotFoundError:
         my_list = []
 
@@ -19,4 +18,4 @@ if __name__ == '__main__':
         if not arg.startswith('./'):
             my_list.append(arg)
 
-    save_to_json_file(my_list, file_name)
+    save_to_json_file(my_list, FILENAME)
