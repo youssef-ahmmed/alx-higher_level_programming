1ef1a7257e8df852fa044e47b618b9e2574cc7aa
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-07-11 22:19:34 +0300

    Edit file name of Mandatory task (7)

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         .load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-    FILENAME = "files/add_item.json"
+    FILENAME = "add_item.json"
     try:
         my_list = load_from_json_file(FILENAME)
     except FileNotFoundError:
