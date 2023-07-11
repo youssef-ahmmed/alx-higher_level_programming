86842d3e6b005be040fcabd172df629da3fee6fc
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-07-11 17:27:59 +0300

    Edit Mandatory task (0)

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,7 @@
 
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, 'r') as f:
+    with open(filename, mode='r', encoding="UTF-8") as f:
         data = f.read()
 
     print(data)
