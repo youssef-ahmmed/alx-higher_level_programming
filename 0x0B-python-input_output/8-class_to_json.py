f3d296e964200dcfbb766d3ddb417d5239ed7c5f
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-07-11 23:06:43 +0300

    Add Mandatory task (8)

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for `class_to_json` function"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary representation of a simple data structure"""
+    return obj.__dic__
