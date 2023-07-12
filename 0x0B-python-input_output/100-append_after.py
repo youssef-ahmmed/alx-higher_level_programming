4f24d3ca0560b0b61022820d181868760e540307
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-07-12 12:41:23 +0300

    Edit Mandatory task (13)

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for `append_after` function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """function that inserts a line of text to a file,
+        after each line containing a specific string """
+    text = ""
+    with open(filename, mode='r', encoding='UTF-8') as file:
+        for line in file:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, mode='w', encoding='UTF-8') as file:
+        file.write(text)
+
+
+append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
+
