21e6b4a530de0454a70c3f09cd57be16b18597ef
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-08 02:19:50 +0300

    Add mandatory task 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+
+SELECT tg.name AS genre, COUNT(tsg.genre_id) AS number_of_shows
+FROM tv_genres AS tg
+INNER JOIN tv_show_genres tsg
+ON tg.id = tsg.genre_id
+GROUP BY tg.name
+ORDER BY number_of_shows DESC;
