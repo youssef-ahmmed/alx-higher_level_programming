f08379c74f1f288ccee769f2f574db23755694a3
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-08 02:26:50 +0300

    Edit mandatory task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,5 @@ SELECT tg.name
 FROM tv_genres AS tg
 INNER JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
 INNER JOIN tv_shows ts ON tsg.show_id = ts.id
-WHERE ts.title = 'Dexter';
+WHERE ts.title = 'Dexter'
+ORDER BY tg.name;
