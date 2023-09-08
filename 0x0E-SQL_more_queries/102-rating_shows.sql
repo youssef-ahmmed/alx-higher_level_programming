497298f6aaa172bcd04e5f2454a6ed98699e7f2e
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-08 03:11:04 +0300

    Add advanced task 19

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- lists all shows from hbtn_0d_tvshows_rate by their rating
+
+SELECT ts.title, SUM(tsr.rate) AS rating
+FROM tv_shows ts
+INNER JOIN tv_show_ratings tsr
+    ON ts.id = tsr.show_id
+GROUP BY ts.title
+ORDER BY rating DESC;
