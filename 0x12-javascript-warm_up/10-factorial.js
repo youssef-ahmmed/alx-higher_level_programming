082fe1924204656ee706e57e91d531a21e4cf4ab
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-11 17:02:08 +0300

    Add mandatory task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (isNaN(num) || num === 0) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(process.argv[2]));
