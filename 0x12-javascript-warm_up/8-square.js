f265cb7e2509a10b805fa834c7804cf84f555154
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-11 16:45:56 +0300

    Add mandatory task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      process.stdout.write('X');
+    }
+    console.log();
+  }
+}
