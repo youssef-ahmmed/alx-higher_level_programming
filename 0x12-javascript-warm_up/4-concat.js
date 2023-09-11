410d6d9540fc96b57469b24b52fd8c33efbad348
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-11 16:16:56 +0300

    Edit mandatory task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-const firstArg = process.argv[2] || undefined;
-const secondArg = process.argv[3] || undefined;
-console.log(firstArg + ' is ' + secondArg);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
