54c9ac0bce696c2ff83ae756728c8b7591e25d89
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-12 14:51:48 +0300

    Edit mandatory task 6: inheritance

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const MainSquare = require('./5-square');
+class Square extends MainSquare {
   charPrint (c) {
     const char = c || 'X';
     for (let i = 0; i < this.width; i++) {
