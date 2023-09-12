d77b67bfad565020695c7ec217a9e66ea7012fe9
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-12 08:41:08 +0300

    Edit mandatory task 7: semistandard

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,10 +3,8 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
 
-  for (let value of list) {
-    if (searchElement === value) {
-      count++;
-    }
+  for (let i = 0; i < list.length; i++) {
+    count = (list[i] === searchElement) ? count + 1 : count;
   }
 
   return count;
