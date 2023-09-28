d1dc823ce754d20555b875ac8ff1eb5157aaa23b
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-28 22:55:00 +0300

    Edit advanced task 8: cat command

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s -X POST -H "Content-Type: application/json" -d @"$1" "$0"
+curl -s -X POST -H "Content-Type: application/json" -d "$(cat "$1")" "$0"
