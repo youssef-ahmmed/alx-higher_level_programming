6f078ffae33f0f0609288d1424764d7d6ae11b4b
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-29 16:02:01 +0300

    Add mandatory task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+
+if __name__ == '__main__':
+
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+    print('Body response:')
+    print(f'\t- type: {type(body)}')
+    print(f'\t- content: {body}')
+    print(f'\t- utf8 content: {body.decode()}')
