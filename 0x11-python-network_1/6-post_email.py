c2d38a542db72f6be4c965c976ef789c9186949a
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-01 18:27:39 +0300

    Add mandatory task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a
+parameter, and displays the body of the response (decoded in utf-8)"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    req = requests.post(argv[1], data={'email': argv[2]})
+    print(req.text)
