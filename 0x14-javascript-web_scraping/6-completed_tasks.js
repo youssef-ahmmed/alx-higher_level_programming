db14d33fc49108f70cd3fb026985db5a8adbf210
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-24 18:24:09 +0300

    Add mandatory task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const todos = JSON.parse(body);
+    const dash = {};
+    for (const todo of todos) {
+      const status = todo.completed;
+      const key = todo.userId;
+      if (status) {
+        if (dash[key]) {
+          dash[key]++;
+        } else {
+          dash[key] = 1;
+        }
+      }
+    }
+    console.log(dash);
+  }
+});
