0271ff18603b32a50610524b88e38aa4b38814bd
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-24 19:00:03 +0300

    Add advanced task 7

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+    for (const character of characters) {
+      request(character, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
