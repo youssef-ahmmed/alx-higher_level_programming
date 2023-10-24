8113cd54a8a6e7611d958e3937bfb604ba717b28
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-24 16:59:57 +0300

    Add mandatory task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
